595fe4dcf628447beb92e7c35f5acdcd36b39335
Jean Amourrouge <papstjl4u@Hedon.Speedport_W_723V_1_49_000>
2024-12-01 17:57:36 +0100

    sumfunct

--- day1/lib/day1.ex
+++ day1/lib/day1.ex
@@ -15,4 +15,16 @@ defmodule Day1 do
   def hello do
     :world
   end
+
+  @spec sum_lists(list(integer()), list(integer()), integer()) :: integer()
+  def sum_lists(a, b, acc \\ 0)
+  def sum_lists([], _, _), do: 0
+  def sum_lists(_, [], _), do: 0
+
+  def sum_lists(a, b, acc) do
+    {f, apop} = List.pop_at(a, 0)
+    {s, bpop} = List.pop_at(b, 0)
+    acc = acc + abs(f - s)
+    sum_lists(apop, bpop, acc)
+  end
 end
