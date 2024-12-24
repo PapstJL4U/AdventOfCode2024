1c5a5bf7a8958ed938eb7ad43314492fef2e9991
Jean Amourrouge <jedi-papstjl@gmx.de>
2024-12-24 19:41:07 +0100

    Day5Part1*

--- day5/lib/day5.ex
+++ day5/lib/day5.ex
@@ -30,9 +30,14 @@ defmodule Day5 do
            97,13,75,29,47
            """
            |> String.split("\n")
+
+  @type row :: {list(integer()), boolean(), integer()}
   def main() do
     input = @example
-    # Adventfile.get_file()
+
+    input =
+      Adventfile.get_simple_input()
+      |> String.split("\n")
 
     sep_index =
       Enum.find_index(input, &(&1 == ""))
@@ -42,21 +47,50 @@ defmodule Day5 do
       |> Enum.map(&String.split(&1, "|", trim: true))
       |> Enum.reject(&(&1 == []))
       |> Enum.map(&inner_parse(&1))
+      |> Enum.map(&List.to_tuple/1)
 
     update =
       Enum.slice(input, (sep_index + 1)..Enum.count(input))
       |> Enum.map(&String.split(&1, ",", trim: true))
       |> Enum.reject(&(&1 == []))
       |> Enum.map(&inner_parse(&1))
+      |> Enum.map(fn x -> {x, true, round((Enum.count(x) - 1) / 2)} end)
+      |> Enum.map(&check_rules(&1, rules))
 
-    IO.inspect(rules)
+    sum =
+      Enum.filter(update, fn {_, correct, _} -> correct == true end)
+      |> Enum.map(fn {input, _, mid} -> Enum.at(input, mid) end)
+      |> Enum.sum()
+
+    # IO.inspect(rules)
     IO.inspect(update)
-
-    IO.puts("~Fine~")
+    IO.inspect(sum)
+    IO.puts("~fine~")
   end
 
   @spec inner_parse(list(String.t())) :: list(integer())
   def inner_parse(list) do
     Enum.map(list, &String.to_integer(&1))
+  end
+
+  @spec check_rules(row, list({integer(), integer()})) :: row
+  def check_rules({input, _, mid}, rules) do
+    correct =
+      Enum.map(rules, &check_rule(input, &1))
+      |> Enum.all?(fn x -> x == true end)
+
+    {input, correct, mid}
+  end
+
+  @spec check_rule(list(integer()), {integer(), integer()}) :: boolean()
+  def check_rule(print, {first, second}) do
+    findex = Enum.find_index(print, &(&1 == first))
+    sindex = Enum.find_index(print, &(&1 == second))
+
+    case {findex, sindex} do
+      {nil, _} -> true
+      {_, nil} -> true
+      {fi, si} -> fi < si
+    end
   end
 end
